1807c34eba0aba3c61763b26dc7b1f3bdeea8b50
Paul Glazzard <Paul.Glazzard@Nology.local>
2020-03-24 14:59:48 +0000

    image updated

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // alert("tsts");
+const toggleHiddenClass = id => {
+  const sectionIds = ["div-1", "div-2", "div-3"];
+  sectionIds.forEach(el =>
+    document.getElementById(el).classList.remove("show")
+  );
+  sectionIds.forEach(el =>
+    el === id ? document.getElementById(id).classList.add("show") : null
+  );
+};
